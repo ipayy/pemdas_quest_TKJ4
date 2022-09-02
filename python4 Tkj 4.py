76160cd7ee339612e77e240c5a0545817c8f914c
ipayy <112527459+ipayy@users.noreply.github.com>
2022-09-02 14:44:03 +0700

    Add files via upload

---
+++ python4 Tkj 4.py
@@ -0,0 +1,64 @@
+# Rumus Persegi Sisi
+
+
+Sisil = 20
+Sisi2 = 20
+Luasp = Sisi1 * Sisi2
+print ("Luas persegi adalah",Luas_p)
+
+
+# Rumus Persegi panjang " Lebar
+
+
+panjang = 10
+Lebar = 20
+Luas_pp = panjang * luas
+
+
+print("Luas Persegi Panjang adalah")
+
+
+# Rumus Trapesium 1/2 * (a+b) * tinggi
+
+
+print ("/nTrapesium")
+a = 6
+b = 10
+t = 8
+luas_t = (a+b) * t / 2
+print ("Luas Trapesium= adalah", luas_t)
+
+
+# Rumus segitiga 1/2 * (a*t)
+
+print ("/nSegitiga")
+a = 10
+t = 8
+luas_t = 1/2 * (a*t)
+print ("Luas segitiga= adalah",luas_s)
+
+
+# Rumus jajar genjang alas * tinggi
+
+
+a = 3
+t = 5
+luas_jg = a * t
+print ("Luas jajar genjang= adalah", luas_jg)
+
+
+# Rumus lingkaran phi * r * r
+
+
+phi = 3.14
+r = 24
+luas_1 = phi * r * r
+print ("Luas lingkaran adalah", luas_1)
+
+
+
+
+
+
+
+ 
